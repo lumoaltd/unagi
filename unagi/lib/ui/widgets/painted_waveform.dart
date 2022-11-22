aad3eba675f36df73b1e882edee5a567cdad599a
Kaisa Piipari <kaisa.piipari@gmail.com>
2022-11-22 13:57:43 +0200

    Create painted_waveform.dart

---
+++ unagi/lib/ui/widgets/painted_waveform.dart
@@ -0,0 +1,13 @@
+class PaintedWaveform extends StatefulWidget {
+  const PaintedWaveform({super.key});
+
+  @override
+  State<PaintedWaveform> createState() => _PaintedWaveformState();
+}
+
+class _PaintedWaveformState extends State<PaintedWaveform> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
